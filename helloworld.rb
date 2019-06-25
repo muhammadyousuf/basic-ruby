f136db873900ba290d6ebed5ba93550f825ffd3a
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-25 13:19:54 +0500

    upcase downcase titlize and capatilize method

--- helloworld.rb
+++ helloworld.rb
@@ -1,5 +1,15 @@
 def saysomething
 puts( "Hello" )
+puts ("muhammad".upcase)
+puts ("yousuf".downcase)
+
+puts ("muhammad yousuf".titleize)
 end
+
+class String
+    def titleize
+      self.split(" ").map{|word| word.capitalize}.join(" ")
+    end
+  end
 
 saysomething
