c6a6ddc32693047f7da8c5de71d083713b781a91
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-26 12:12:22 +0500

    add two classes

--- classConstructor.rb
+++ classConstructor.rb
@@ -1,8 +1,18 @@
+class Thing
+  def saysomething(name, description)
+    @name = name
+    @description = description
+    puts 'My cat name is ' + @name + ' and ' + @description
+  end 
+end
 class Teasure
-  def initialize
-   puts 'Hello teasure'
+  def initialize(name)
+  puts name
   end
 end
 
-Teasure.new
+obj = Thing.new
+Teasure.new('muhammad yousuf')
+obj.saysomething('tom', 'it is very beautiful')
+
 
