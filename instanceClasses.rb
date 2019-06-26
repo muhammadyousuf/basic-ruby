cf634f72943cff0877910fccd2c25d5e2649e510
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-26 11:23:56 +0500

    class instance created

---
+++ instanceClasses.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def set_name(aName)
+   @myname = aName
+  end
+
+  def get_name
+    @myname
+  end
+
+end
+
+myDog = Dog.new
+
+yourDog = Dog.new
+
+myDog.set_name('puppy')
+
+yourDog.set_name('tommy')
+
+puts 'My Dog name is ' + myDog.get_name
+
+puts 'Your Dog name is ' + yourDog.get_name
