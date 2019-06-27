23893293fda296461cb135f2ad8899b5429a9af1
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-27 10:36:33 +0500

    override child class method

---
+++ classInheritance2.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Parent
+  def initialize
+    puts 'hello parents'
+  end
+
+  def user
+    puts 'hello parent user'
+  end
+end
+
+class Child
+  def initialize
+    puts 'hello child'
+  end
+
+  def user
+    puts 'hello child user'
+  end
+end
+
+Parent.new
+
+obj = Child.new
+
+obj.user
