fe73d84ebbec1692f80e3346fad320d6ac27913f
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-28 11:17:22 +0500

    first array program

---
+++ chapter 5/array1.rb
@@ -0,0 +1,19 @@
+def saysomething
+  'hello world'
+end
+
+arr2 = ['a', 4, 4.6, saysomething]
+
+puts arr2[0]
+
+puts arr2[1]
+
+puts arr2[-1]
+
+puts arr2[-2]
+
+puts arr2[3]
+
+puts arr2[2]
+
+puts arr2
