c9dc72d18ee9854280a5198dce893665c55be0c9
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-06-28 11:28:59 +0500

    array expresion and directory

---
+++ chapter 5/array2.rb
@@ -0,0 +1,28 @@
+mymap = %w(room1 room2 room3)
+
+puts mymap
+
+arr = ['one', 'two', 'three', 'four']
+
+puts arr[0]
+
+puts arr[1]
+
+puts arr[2]
+
+puts arr[3]
+
+puts arr[4]
+
+
+def hello 
+  'hello world'
+end
+
+x = [1+4, hello, `dir`]
+
+puts x[2]
+
+puts x[-2]
+
+puts x[0]
