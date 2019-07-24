2cedc11a98dece8f9506f822b94d187b370329e9
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-07-24 14:41:25 +0500

    intersection hashes

---
+++ chapter 6/operation_hashes.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+h1 = { 'key1' => 'val1', 'key2' => 'val2', 'key3' => 'val3', 'key4' => 'val4' }
+h2 = { 'key1' => 'val1', 'KEY_TWO' => 'val2', 'key3' => 'VALUE_3',
+       'key4' => 'val4' }
+p(h1.keys & h2.keys)
