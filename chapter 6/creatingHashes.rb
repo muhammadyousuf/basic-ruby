33f7989fb35c8835f4e7934eb978ae1512bd882f
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-07-24 13:40:15 +0500

    start hashes

---
+++ chapter 6/creatingHashes.rb
@@ -0,0 +1,11 @@
+h1 = Hash.new
+h2 = Hash.new("Some kind of ring")
+
+h2['treasure1'] = 'Silver ring'
+h2['treasure2'] = 'Gold ring'
+h2['treasure3'] = 'Ruby ring'
+h2['treasure4'] = 'Sapphire ring'
+
+puts h1
+
+puts h2
